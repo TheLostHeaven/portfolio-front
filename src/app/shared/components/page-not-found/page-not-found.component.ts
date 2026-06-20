c86b9df76406c3845cf58024eca45217d71c1432
Daniel <dannyking07@outlook.es>
2026-06-20 17:35:48 -0500

    feat(shared): add page-not-found component

---
+++ src/app/shared/components/page-not-found/page-not-found.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: false,
+  selector: 'app-page-not-found',
+  templateUrl: './page-not-found.component.html',
+  styleUrl: './page-not-found.component.scss',
+})
+export class PageNotFoundComponent {}
