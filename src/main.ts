e315ecf567a167ecd365bfd51110479e39f7b433
Daniel <dannyking07@outlook.es>
2026-06-20 17:37:11 -0500

    chore(app): update bootstrap strategy and add global styling

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { App } from './app/app';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { LayoutModule } from './app/layout/layout.module';
 
-bootstrapApplication(App, appConfig)
+platformBrowserDynamic().bootstrapModule(LayoutModule)
   .catch((err) => console.error(err));
