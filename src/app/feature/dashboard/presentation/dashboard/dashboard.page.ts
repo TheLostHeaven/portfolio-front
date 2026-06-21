62894bb0248853ca39ab923766eded113cc1d202
Daniel <dannyking07@outlook.es>
2026-06-20 20:21:18 -0500

    feat(dashboard): add dashboard feature module with routing

---
+++ src/app/feature/dashboard/presentation/dashboard/dashboard.page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: false,
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.page.html',
+  styleUrl: './dashboard.page.scss',
+})
+export class Dashboard {}
